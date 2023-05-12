bb62bf004817d26132ec4ef43ee2bebfbccd1cfc
justmore5mins <standby-nexuses0m@icloud.com>
2023-05-12 18:15:56 +0800

    add

--- datacollecter/screen.py
+++ datacollecter/screen.py
@@ -1,2 +1,12 @@
-import pyautogui
+from pyautogui import screenshot
 
+class screendata:
+    screenshot:str = "data"
+    def __init__(self) -> None:
+        pass
+    
+    def takescreenshot(self):
+        shots = screenshot()
+        shots.save(self.screenshot)
+        
+datas = screendata().takescreenshot()
