c401e9cb4eeb65a3af25110c37629fc795d39a95
justmore5mins <standby-nexuses0m@icloud.com>
2023-05-13 07:28:15 +0800

    add

--- autopusher.py
+++ autopusher.py
@@ -1,5 +1,6 @@
 from os import system
 from time import sleep as delay
+from datetime import datetime
 
 folder = "/Users/AustinYu/Library/CloudStorage/GoogleDrive-tno062789@gmail.com/我的雲端硬碟/python/computer data collecter"
 repository = "https://github.com/justmore5mins/computer-data-collecter.git"
@@ -12,7 +13,9 @@ commands = [
 ]
 
 while True:
+    print("\n")
     print("start push codes")
+    print("nowtime:",datetime.now())
     for command in commands:
         system(command)
     
