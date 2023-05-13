420511efd1f77317c4f8061ae429c8f830399e1d
justmore5mins <standby-nexuses0m@icloud.com>
2023-05-13 11:55:25 +0800

    add

--- Unified/frame.py
+++ Unified/frame.py
@@ -1,1 +1,7 @@
-from 
+from ..analyser import getframesize
+
+class unified:
+    output:str
+    shotpath:str
+    def __init__(self,output:str = "/Users/AustinYu/Library/CloudStorage/GoogleDrive-tno062789@gmail.com/我的雲端硬碟/python/computer data collecter/data/output.json",shotpath:str = "/Users/AustinYu/Library/CloudStorage/GoogleDrive-tno062789@gmail.com/我的雲端硬碟/python/computer data collecter/datacollecter/data/shot.png") -> None:
+        pass
