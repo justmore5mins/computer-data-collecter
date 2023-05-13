b08192b18da17ac2a09eb018420cdb4be4a5fdf1
justmore5mins <standby-nexuses0m@icloud.com>
2023-05-13 09:38:52 +0800

    add

--- datacollecter/screen.py
+++ datacollecter/screen.py
@@ -2,10 +2,10 @@ from PIL.ImageGrab import grab
 
 class screendata:
     shotpath:str
-    def __init__(self,shotpath:str) -> None:
+    def __init__(self,shotpath:str = "data/shot.png") -> None:
         self.shotpath = shotpath
     
     def takescreenshot(self):
         grab().save(self.shotpath)
-
-screendata("data/shot.png").takescreenshot()
+        
+screendata().takescreenshot()
